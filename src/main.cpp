c79dd6721927c6a4533c2fe6fd501a3fc7ff6a7c
Jeremy Urena <jau@njit.edu>
2025-03-07 00:15:14 -0500

    Added WiFi connectivity to my ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,47 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include <WiFiMulti.h>
+#include <WiFi.h>
+#define LED_BUILTIN 2
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "casa";
+const char* password = "91-plum-1724";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+WiFiMulti wifimulti;
+
+void setup(){
+    Serial.begin(921600);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    WiFi.begin(ssid, password);
+
+    Serial.println("Starting");
+
+    /*wifimulti.addAP(ssid, password);
+    
+    while (wifimulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    Serial.println("Connected!");*/
+    
 }
+
+bool isConnected = false;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    if (WiFi.status() == WL_CONNECTED && !isConnected) {
+        Serial.println("Connected!");
+        digitalWrite(LED_BUILTIN, HIGH);
+        isConnected = true;
+    }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println(".");
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+        delay(1000);
+        isConnected = false;
+    }
+
+    //digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
+
 }
