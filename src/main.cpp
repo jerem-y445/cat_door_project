f0ec8b85c8dd512e4e39b5bcdd6969ffb4db072e
Jeremy Urena <116043120+jero-me1@users.noreply.github.com>
2025-03-08 15:04:22 -0500

    Added notes for the project

--- src/main.cpp
+++ src/main.cpp
@@ -50,11 +50,11 @@ void loop() {
         for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
             // in steps of 1 degree
             myServo.write(pos);    // tell servo to go to position in variable 'pos'
-            delay(15);             // waits 15ms for the servo to reach the position
+            delay(1);             // waits 15ms for the servo to reach the position
         }
         for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
             myServo.write(pos);    // tell servo to go to position in variable 'pos'
-            delay(15);
+            delay(1);
         }
         oneSweep++;
     }
