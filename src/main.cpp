d8900b4c2a15fd1b7fb69d3cb1a9a6c26ee0eb96
jerem-y445 <jau@njit.edu>
2025-03-10 18:04:54 -0400

    Started to define conditions for each state (inside or outside)

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 #include <ESP32Servo.h>
 #define LED_BUILTIN 2
 
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "<insert wifi name here>";
+const char* password = "<insert wifi password here>";
 
-Servo myServo;
+Servo servo1, servo2;
 int pos = 0;
-int servoPin = 18;
+int servoPin1 = 18;
+int servoPin2 = 12;
 
 void setup(){
     
@@ -24,13 +25,15 @@ void setup(){
 	ESP32PWM::allocateTimer(1);
 	ESP32PWM::allocateTimer(2);
 	ESP32PWM::allocateTimer(3);
-	myServo.setPeriodHertz(50);
-	myServo.attach(servoPin, 500, 2400);
+	servo1.setPeriodHertz(50);
+	servo1.attach(servoPin1, 500, 2400);
+    servo2.setPeriodHertz(50);
+	servo2.attach(servoPin2, 500, 2400);
 
 }
 
-bool isConnected = false;
-int oneSweep = 0;
+//bool isConnected = false;
+bool isOutside = false;
 
 void loop() {
     /*if (WiFi.status() == WL_CONNECTED && !isConnected) {
@@ -45,18 +48,18 @@ void loop() {
         //delay(1000);
         isConnected = false;
     }*/
-    
-    while(oneSweep < 1) {
-        for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-            // in steps of 1 degree
-            myServo.write(pos);    // tell servo to go to position in variable 'pos'
-            delay(15);             // waits 15ms for the servo to reach the position
+    if (hallRead() > 1 && !isOutside) {
+        for (pos = 0; pos <= 180; pos++) {
+            servo1.write(pos);
+            delay(10);
+            servo2.write(180 - pos);
+            delay(10);
         }
-        for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-            myServo.write(pos);    // tell servo to go to position in variable 'pos'
-            delay(15);
-        }
-        oneSweep++;
+        Serial.println("Gato is inside; door is locked");
+    }
+
+    if (hallRead() < 1) {
+        Serial.println("Gato is outside; door is unlocked");
     }
 
 }
